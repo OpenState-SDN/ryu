df1ac980482af4e9313382ace7c8d20caba7f38e
Davide Sanvito <dsanvito90@gmail.com>
2015-10-23 08:41:05 -0700

    run_all.sh script: added summary

--- ryu/app/openstate/test/run_all.sh
+++ ryu/app/openstate/test/run_all.sh
@@ -1,1 +1,19 @@
-for f in *.py; do echo -e "\n[Testing \x1b[32m$f\x1b[0m]"; sudo python $f; done
+declare -A ret_values
+
+for f in *.py;
+do
+	echo -e "\n\x1b[33m[Testing $f]\x1b[0m";
+	sudo python $f;
+	ret_values[$f]=$?;
+done 
+
+echo -e "\n*******************************************************************\n\nSUMMARY:\n"
+
+for f in *.py;
+do
+	if [[ ${ret_values[$f]} -eq 1 ]]; then
+		echo -e "$f: \x1b[31mFAILED\x1b[0m";
+	else
+		echo -e "$f: \x1b[32mSUCCEEDED\x1b[0m";
+	fi
+done 
